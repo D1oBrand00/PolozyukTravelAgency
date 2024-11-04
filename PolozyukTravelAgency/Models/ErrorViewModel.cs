0961080c5d8777d25e4c5232955659bbca2ed6f4
D1oBrand00 <danikpolzyuk@gmail.com>
2024-11-04 08:53:40 +0300

    Добавление проекта

---
+++ PolozyukTravelAgency/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PolozyukTravelAgency.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
